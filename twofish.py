1f776ffc2642e93ef73e683d5786194c6f8f67db
Fatima Kammona <yourgitemail@example.com>
2023-11-21 17:59:03 -0500

    Divided the key into the correct arrays for creating s0 and s1 boxes.

--- twofish.py
+++ twofish.py
@@ -13,7 +13,26 @@ def input_from_user_key(prompt):
         else:
             print("Error: Please enter a valid hex string of 128, 192, or 256 bits in length.")
 
+def key_schedule_s(key):
+    RS_matrix = [
+    [0x01, 0xA4, 0x55, 0x87, 0x5A, 0x58, 0xDB, 0x9E],
+    [0xA4, 0x56, 0x82, 0xF3, 0x1E, 0xC6, 0x68, 0xE5],
+    [0x02, 0xA1, 0xFC, 0xC1, 0x47, 0xAE, 0x3D, 0x19],
+    [0xA4, 0x55, 0x87, 0x5A, 0x58, 0xDB, 0x9E, 0x03]
+    ]
+
+    # Break up the key by twos and convert to integers
+    key_array = [key[i:i+2] for i in range(0, len(key), 2)]
+    key_length = int(len(key_array) / 2) 
+
+    key_first_half = key_array[:key_length]
+    key_second_half = key_array[key_length:]
+
+def key_schedule(key):
+    key_schedule_s(key)
+
 if __name__ == "__main__":
     key = input_from_user_key("Enter a hex string of 128, 192, or 256 bits in length: ")
     print(f"key {key}")
+    key_schedule(key)
     
