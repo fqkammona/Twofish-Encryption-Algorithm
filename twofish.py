39cb607cc2f4eb0210c717fa4baead7412ba89f3
Fatima Kammona <yourgitemail@example.com>
2023-11-21 17:08:24 -0500

    Key takes in 128, 192, or 256 hex bit.

--- twofish.py
+++ twofish.py
@@ -2,18 +2,18 @@
 #Class: CS 4980 Cryptography 
 #Name: Fatima Kammona
 
-def input_from_user(prompt, length):
+def input_from_user_key(prompt):
     while True:
-        user_input = input(prompt)
-        if all((letter in "01") for letter in user_input) and len(user_input) == length:
+        user_input = input(prompt).replace(" ", "").lower()  # Remove spaces and convert to lowercase
+        hex_length = len(user_input)
+    
+        # Check if the length of the string is 32 (128-bit), 48 (192-bit), or 64 (256-bit) characters
+        if hex_length in (32, 48, 64) and all(c in '0123456789abcdef' for c in user_input):
             return user_input
-        print(f"Error: Please enter a {length}-bit binary number.")
-
-def print_summary(key, plaintext):
-    print(f"Key: {key}")
-    print(f"PT: {plaintext}")
+        else:
+            print("Error: Please enter a valid hex string of 128, 192, or 256 bits in length.")
 
 if __name__ == "__main__":
-    key = input_from_user("Enter 32-bit Key: ", 32)
-    plaintext = input_from_user("Enter 32-bit Plantext: ", 32)
+    key = input_from_user_key("Enter a hex string of 128, 192, or 256 bits in length: ")
+    print(f"key {key}")
     
